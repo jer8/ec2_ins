3343c5bed664806b21cc3db453a47a93ba8c891c
Arnav Jaret <74366975+jer8@users.noreply.github.com>
2025-10-05 19:46:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
 import os
 import uvicorn
-
+#uvicorn form FasAPI server
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from vocode.streaming.models.telephony import TwilioConfig
@@ -151,3 +151,4 @@ async def inbound_call(request: Request):
 # Run server
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=3000)
+
